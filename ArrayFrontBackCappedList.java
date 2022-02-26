81d01435641e7f2ae425dd95be88f4282be64641
Chris Tolan <69782006+tolanbot@users.noreply.github.com>
2022-02-26 00:52:32 -0800

    Update ArrayFrontBackCappedList.java

--- ArrayFrontBackCappedList.java
+++ ArrayFrontBackCappedList.java
@@ -5,7 +5,7 @@
  * Duplicate entries are allowed.
  * 
  * @author Chris Tolan
- * @version 1.0
+ * @version 2.0
  */
 public class ArrayFrontBackCappedList<T> implements FrontBackCappedListInterface<T> {
     private T[] list;
